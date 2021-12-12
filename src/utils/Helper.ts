993648bc525213253f72683bbf1559c4d9c94a67
harrytang <harry@powerkernel.com>
2021-12-12 16:48:31 +0200

    feat: add random number generation

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -10,4 +10,11 @@ export default class Helper {
   public static uuidv4(): string {
     return uuidv4();
   }
+
+  public static randomNumber(min: number, max: number): number {
+    if (min > max) {
+      throw new Error("min cannot be greater than max");
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
--- src/utils/__tests__/Helper.test.ts
+++ src/utils/__tests__/Helper.test.ts
@@ -11,3 +11,19 @@ it("returns uuid v4", () => {
   const uuid = Helper.uuidv4();
   expect(validator.isUUID(uuid, 4)).toBe(true);
 });
+
+it("returns a random number within the range of min-max", () => {
+  const num1 = Helper.randomNumber(1, 10);
+  expect(num1).toBeGreaterThanOrEqual(1);
+  expect(num1).toBeLessThanOrEqual(10);
+
+  const num2 = Helper.randomNumber(100000, 999999);
+  expect(num2).toBeGreaterThanOrEqual(100000);
+  expect(num2).toBeLessThanOrEqual(999999);
+});
+
+it("thorws an error if min is greater than max", () => {
+  expect(() => {
+    Helper.randomNumber(10, 5);
+  }).toThrow();
+});
