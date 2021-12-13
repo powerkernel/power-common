080da15bc7e91369c1da532705c68442926c0806
harrytang <harry@powerkernel.com>
2021-12-13 22:50:04 +0200

    feat: add entity abstract class

---
+++ src/abstracts/entity.ts
@@ -0,0 +1,45 @@
+/**
+ * @author Harry Tang <harry@powerkernel.com>
+ * @link https://powerkernel.com
+ * @copyright Copyright (c) 2021 Power Kernel
+ */
+
+import Validator from "./../utils/validator";
+
+abstract class Entity {
+  protected wrappedId!: string;
+  protected wrappedCreatedAt!: string;
+  protected wrappedUpdatedAt!: string;
+
+  public get id(): string {
+    return this.wrappedId;
+  }
+  public set id(id: string) {
+    if (!Validator.isUUID(id)) {
+      throw new Error("Invalid id");
+    }
+    this.wrappedId = id;
+  }
+
+  public get createdAt(): string {
+    return this.wrappedCreatedAt;
+  }
+  public set createdAt(date: string) {
+    if (!Validator.isISODate(date)) {
+      throw new Error("Invalid date");
+    }
+    this.wrappedCreatedAt = date;
+  }
+
+  public get updatedAt(): string {
+    return this.wrappedUpdatedAt;
+  }
+  public set updatedAt(date: string) {
+    if (!Validator.isISODate(date)) {
+      throw new Error("Invalid date");
+    }
+    this.wrappedUpdatedAt = date;
+  }
+}
+
+export default Entity;
